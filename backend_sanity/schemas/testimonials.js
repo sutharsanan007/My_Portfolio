70779dba44ffeba575083471f2c5f5226b2c5d50
sutharsanan007 <sutharsananravi@gmail.com>
2023-07-27 19:09:15 -0400

    Final Update

---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,31 @@
+export default {
+    name:'testimonials',
+    title:'Testimonials',
+    type: 'document',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
+            type: 'string'
+        },
+        {
+            name:'company',
+            title:'Company',
+            type:'string'
+        },
+        {
+            name:'imgUrl',
+            title:'ImgUrl',
+            type: 'image',
+            imgurl: 'imgUrl',
+            options: {
+              hotspot: true,
+            },
+        },  
+        {
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
+        }
+    ]
+}
