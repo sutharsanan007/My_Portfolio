44bc1a68549bec3674a5e31c38e596efe3466d46
sutharsanan007 <sutharsananravi@gmail.com>
2023-08-17 16:11:55 -0400

    Final Changes

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -35,6 +35,7 @@ const Header = () => (
         <div className="tag-cmp app__flex">
           <p className="p-text">Web Developer</p>
           <p className="p-text">Data Analyst</p>
+          <p className="p-text">IT Support</p>
           <p className="p-text">Open To Work</p>
         </div>
       </div>
